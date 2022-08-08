2f948b9bb938f7683693a686657a85a042144632
Dmitry Lopatinsky <lopatinskyda@gmail.com>
2022-08-09 01:11:15 +0400

    finished first homework

--- Seminar_1_Family_tree/src/App.java
+++ Seminar_1_Family_tree/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Person person = new Person("Vasiliy", "Kochkin", "male",42);
         Person person1= new Person("Dmitry", "Kochkin", "male",20);
         Person person2 = new Person("Petr", "Kochkin","male",14);
@@ -14,12 +14,13 @@ public class App {
         tree.addLink(person1, person, Relation.Child, Relation.Parent);
         tree.addLink(person2, person, Relation.Child, Relation.Parent);
         tree.addLink(person, person3, Relation.Husband, Relation.Wife);
-       //tree.addLink(person2, person1, Relation.Brother, Relation.Brother);
+        tree.addLink(person2, person1, Relation.Brother, Relation.Brother);
+        tree.addLink(person6, person5, Relation.Sister, Relation.Brother);
 
 
 //        Research.printAllChildren("Vasiliy", "Kochkin", tree);
 //        Research.printWifeOrHusband("Olga", "Kochkina", tree);
-        Research.findBrothersAndSisters("Dmitry", "Kochkin", tree);
+        Research.findBrothersAndSisters("Dmitriy", "Ivanov", tree);
 
 
     }
--- Seminar_1_Family_tree/src/Research.java
+++ Seminar_1_Family_tree/src/Research.java
@@ -1,36 +1,36 @@
 import java.util.List;
 
 public class Research {
-  /*Метод вывода списка всех детей человека*/
-    public static void printAllChildren( String name, String surname, Tree tree){
+    /* Метод вывода списка всех детей человека */
+    public static void printAllChildren(String name, String surname, Tree tree) {
         List<Link> links = tree.getLinks();
 
-        for (Link el : links){
-            if (el.getPersonFirst().getName().equals(name) & el.getPersonFirst().getSurname().equals(surname) && el.getLink() == Relation.Parent) {
+        for (Link el : links) {
+            if (el.getPersonFirst().getName().equals(name) & el.getPersonFirst().getSurname().equals(surname)
+                    && el.getLink() == Relation.Parent) {
                 System.out.println(el.getPersonSecond());
             }
         }
     }
-/*метод выводит мужа или жену*/
-    public static void printWifeOrHusband(String name, String surname, Tree tree){
+
+    /* метод выводит мужа или жену */
+    public static void printWifeOrHusband(String name, String surname, Tree tree) {
         List<Link> links = tree.getLinks();
 
-        for (Link el : links){
-            if (el.getPersonFirst().getName().equals(name) && el.getPersonFirst().getSurname().equals(surname) && el.getLink() == Relation.Wife | el.getLink() == Relation.Husband)
+        for (Link el : links) {
+            if (el.getPersonFirst().getName().equals(name) && el.getPersonFirst().getSurname().equals(surname)
+                    && el.getLink() == Relation.Wife | el.getLink() == Relation.Husband)
                 System.out.println(el.getPersonSecond());
         }
     }
 
-    public static void findBrothersAndSisters(String name, String surname, Tree tree){
+    public static void findBrothersAndSisters(String name, String surname, Tree tree) {
         List<Link> links = tree.getLinks();
-        for (Link el: links){
-            if(el.getPersonFirst().getName().equals(name) && el.getPersonFirst().getSurname().equals(surname) && el.getLink() == Relation.Child){
-                Object parent = el.getPersonSecond();
-                System.out.println(parent);
+        for (Link el : links) {
+            if (el.getPersonFirst().getName().equals(name) && el.getPersonFirst().getSurname().equals(surname)
+                    && el.getLink() == Relation.Brother | el.getLink() == Relation.Sister) {
+                System.out.println(el.getPersonSecond());
             }
         }
-//        for (Link el: links){
-//
-//        }
     }
 }
